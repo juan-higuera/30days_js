37b7fa2841551b9fe983d4ce043c6fa318100c5e
Juan Felipe Higuera <163941029+juan-higuera@users.noreply.github.com>
2024-10-24 23:44:29 -0500

    dia 3 continuacion
    (me enferme pero ya mejore e vuelto )

--- day 3/ejercicios2.js
+++ day 3/ejercicios2.js
@@ -38,11 +38,45 @@
 // console.log("Tu circunferencia es de :  ",circunferencia)
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
-// Esta hecho para cumplir la orden sin embargo se requiere mejora :D 
+// Esta hecho para cumplir la orden sin embargo no sigue una que este en desorden :D 
 
-y=prompt("Dame la ecuacion expresada") //2*x -2
-a= y.search("x")
-b= y.search(/\*/)
-if (a>b){c=y.substring(0,a-1)} else {c=y.substring(b+1)}
-console.log(c)
-console.log("Corte con eje y: ", y.match(/([+-]\d+)/g))
+// y=("2*x-2") //2*x-2
+// a= y.search("x")
+// b= y.search(/\*/)
+// if (a>b){ex2=y.substring(0,a-1)} else {ex2=y.substring(b+1)}
+// console.log(ex2)
+// console.log("Corte con eje y: ", y.match(/([+-]\d+)/g))
+
+// la de abajo encuentra la pendiente pero las 3 deben estar sin comentarios para que funcione-> up an down
+
+// function pendiente(x1,y1,x2,y2) {
+//     m = (y2-y1)/(x2-x1)
+//     console.log(m)
+//     return m
+// }
+
+// aca los comparo ////////////////////
+
+// ex1 = pendiente(2,2,6,10);
+// console.log(ex1==ex2) /* tener en cuenta que una es un string y la otra un int */
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+function operacion(nInicial){
+    console.log(["x","y"])
+    for (let x = nInicial,r=0;r<=6; x++,r++){
+        y = x**2 + 6*x + 9
+        console.log([x,y])
+    }
+}
+operacion(-6)
+console.log("//////////////////////////////////////////")
+operacion(-3)
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// a diferencia de los anteriores este fue bueno... no se quizas me saltare estos tipos de ejercicios.. fin del dia 3
+
+// console.log("Dame tus numeros");
+// let b= Number(prompt("Ingrese horas: "));
+// let c= Number(prompt("Introduce la tarifa por hora: "));
+// let d= (b+c)
+// console.log(d)
